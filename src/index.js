b05cf4032549c8a29657625c9ab7ead33853deaa
Dmitry K <69163014+webdev163@users.noreply.github.com>
2021-08-18 01:18:53 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsPairs = [];
+  bracketsConfig.forEach(el => !isNaN(el[0] || el[1]) ? bracketsPairs.push(el[0] + el[1]) : bracketsPairs.push('\\' + el[0] + '\\' + el[1]));
+  let result = str
+  for (i = 0; i < str.length; i++) {
+    for (j = 0; j < bracketsPairs.length; j++) {
+      result = result.replace(new RegExp(bracketsPairs[j], 'g'), '');
+    }
+  }
+  return result.length === 0 ? true : false
 }
